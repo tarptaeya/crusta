22e61d5fd6e0fcacd4ed9907057f5cea9adde7b8
Anmol Gautam <tarptaeya@gmail.com>
2017-10-07 01:48:20 +0530

    Add files via upload

--- windows_updater/updater.ps1
+++ windows_updater/updater.ps1
@@ -8,12 +8,22 @@ $newfiletext=[System.IO.File]::ReadAllText("new_version.txt")
 $equal = $currentfiletext -ceq $newfiletext
 
 if($equal -eq $false){
-    $choice=(New-Object -ComObject Wscript.Shell).Popup('A new version of Crusta is available. Do you want to install new Crusta now?',0,'Crusta Updater',0x4)
+    $webclient.DownloadFile("http://crustabrowser.com/version/release_notes.txt","releasenotes.txt")
+    $releasenotestext = [System.IO.File]::ReadAllText("releasenotes.txt")
+    $choice=(New-Object -ComObject Wscript.Shell).Popup('A new version of Crusta is available. Do you want to install new Crusta now?
+
+Local version of Crusta   : '+$currentfiletext+'
+Current version of Crusta : '+$newfiletext+'
+
+New in this version :
+
+'+$releasenotestext,0,'Crusta Updater',0x4)
     if ($choice -eq 6){
-         Start-BitsTransfer http://crustabrowser.com/version/setup.exe setup.exe
-         $run = (New-Object -ComObject Wscript.Shell).Popup('Downloaded setup file - Do you want to run it now?',0,'Crusta Updater',0x0)
-         if($run -eq 1){
+        Start-BitsTransfer http://crustabrowser.com/version/setup.exe setup.exe
+        $run = (New-Object -ComObject Wscript.Shell).Popup('Downloaded setup file - Do you want to run it now?',0,'Crusta Updater',0x0)
+        if($run -eq 1){
+            uninstaller.exe
             setup.exe
-         }
+        }
     }
 }
