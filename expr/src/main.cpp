4cef5cf325386c4c8ae24336c26c7c35d8274f7c
Anmol Gautam <tarptaeya@gmail.com>
2019-05-05 14:46:06 +0530

    dont use native styles on mac

--- expr/src/main.cpp
+++ expr/src/main.cpp
@@ -1,11 +1,16 @@
 #include "browser.h"
 #include <QApplication>
+#include <QStyleFactory>
 
 int main(int argc, char **argv)
 {
     QApplication app(argc, argv);
+    // don't use native style on macos by default
+    app.setStyle(QStyleFactory::create(QStringLiteral("fusion")));
+
     Browser::instance()->run();
     int retCode = app.exec();
     Browser::instance()->deleteLater();
+
     return retCode;
 }
