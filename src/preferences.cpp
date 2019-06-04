9700ae297d5597f9b3cdfbd632f143e76f4b0457
Anmol Gautam <tarptaeya@gmail.com>
2019-06-04 19:03:26 +0530

    added frontend for web attr settings

--- src/preferences.cpp
+++ src/preferences.cpp
@@ -10,6 +10,7 @@
 #include <QLineEdit>
 #include <QListWidget>
 #include <QPushButton>
+#include <QScrollArea>
 #include <QSettings>
 #include <QSpinBox>
 #include <QTabWidget>
@@ -22,21 +23,13 @@ Preferences::Preferences(QWidget *parent)
 {
     m_tabWidget = new QTabWidget;
 
-    QPushButton *doneButton = new QPushButton(QStringLiteral("Done"));
-
     QVBoxLayout *vboxLayout = new QVBoxLayout;
     setLayout(vboxLayout);
 
     vboxLayout->addWidget(m_tabWidget);
 
-    QHBoxLayout *hboxLayout = new QHBoxLayout;
-    hboxLayout->addWidget(new QWidget, 1);
-    hboxLayout->addWidget(doneButton, 0);
-    vboxLayout->addLayout(hboxLayout);
-
     m_tabWidget->setDocumentMode(true);
     m_tabWidget->tabBar()->setExpanding(true);
-    doneButton->setObjectName(QStringLiteral("default"));
 
     installWidget(QStringLiteral("Appearance"), createAppearanceTab());
     installWidget(QStringLiteral("Browsing"), createBrowsingTab());
@@ -45,17 +38,10 @@ Preferences::Preferences(QWidget *parent)
     installWidget(QStringLiteral("Privacy"), createPrivacyTab());
 
     setAttribute(Qt::WA_DeleteOnClose);
-#ifdef __APPLE__
-    setWindowFlags(Qt::FramelessWindowHint | Qt::WindowStaysOnTopHint);
-#else
     setWindowTitle(QStringLiteral("Crusta:Preferences"));
     setWindowFlags(Qt::WindowStaysOnTopHint);
-#endif
-
-    Utils::setMovableByBackground(winId());
 
     setMinimumWidth(100 * m_tabWidget->count());
-    connect(doneButton, &QPushButton::clicked, this, &QWidget::close);
 }
 
 void Preferences::installWidget(const QString &name, QWidget *widget)
@@ -271,6 +257,92 @@ QWidget *Preferences::createWebEngineTab()
     QWidget *widget = new QWidget;
     QVBoxLayout *vboxLayout = new QVBoxLayout;
     widget->setLayout(vboxLayout);
+
+    QScrollArea *area = new QScrollArea;
+    {
+        QWidget *widget = new QWidget;
+        QVBoxLayout *vboxLayout = new QVBoxLayout;
+        widget->setLayout(vboxLayout);
+        area->setWidgetResizable(true);
+        area->setWidget(widget);
+        area->setContentsMargins(0, 0, 0, 0);
+
+        QCheckBox *autoLoadImages = new QCheckBox(QStringLiteral("Auto load images"));
+        QCheckBox *javaScriptEnabled = new QCheckBox(QStringLiteral("JavaScript enabled"));
+        QCheckBox *javaScriptCanOpenWindows = new QCheckBox(QStringLiteral("JavaScript can open windows"));
+        QCheckBox *javaScriptCanAccessClipboard = new QCheckBox(QStringLiteral("JavaScript can access clipboard"));
+        QCheckBox *linksIncludedInFocusChain = new QCheckBox(QStringLiteral("Links included in focus chain"));
+        QCheckBox *localStorageEnabled = new QCheckBox(QStringLiteral("Local storage enabled"));
+        QCheckBox *localContentCanAccessRemoteUrls = new QCheckBox(QStringLiteral("Local content can access remote urls"));
+        QCheckBox *xssAuditingEnabled = new QCheckBox(QStringLiteral("XSS auditing enabled"));
+        QCheckBox *spatialNavigationEnabled = new QCheckBox(QStringLiteral("Spatial navigation enabled"));
+        QCheckBox *localContentCanAccessFileUrls = new QCheckBox(QStringLiteral("Local content can access file urls"));
+        QCheckBox *hyperLinkAuditingEnabled = new QCheckBox(QStringLiteral("Hyperlink auditing enabled"));
+        QCheckBox *scrollAnimatorEnabled = new QCheckBox(QStringLiteral("Scroll animator enabled"));
+        QCheckBox *errorPageEnabled = new QCheckBox(QStringLiteral("Error page enabled"));
+        QCheckBox *pluginsEnabled = new QCheckBox(QStringLiteral("Plugins enabled"));
+        QCheckBox *fullScreenSupportEnabled = new QCheckBox(QStringLiteral("Full screen support enabled"));
+        QCheckBox *screenCaptureEnabled = new QCheckBox(QStringLiteral("Screen capture enabled"));
+        QCheckBox *webglEnabled = new QCheckBox(QStringLiteral("WebGL enabled"));
+        QCheckBox *accelerated2DCanvasEnabled = new QCheckBox(QStringLiteral("Accelerated 2D canvas enabled"));
+        QCheckBox *autoLoadIconsForPage = new QCheckBox(QStringLiteral("Auto load icons for page"));
+        QCheckBox *touchIconsEnabled = new QCheckBox(QStringLiteral("Touch icons enabled"));
+        QCheckBox *focusOnNavigationEnabled = new QCheckBox(QStringLiteral("Focus on navigation enabled"));
+        QCheckBox *printElementBackgrounds = new QCheckBox(QStringLiteral("Print elements background"));
+        QCheckBox *allowRunningInsecureContent = new QCheckBox(QStringLiteral("Allow running insecure content"));
+        QCheckBox *allowGeolocationOnInsecureOrigin = new QCheckBox(QStringLiteral("Allow geolocation on insecure origin"));
+        QCheckBox *allowWindowActivationFromJavaScript = new QCheckBox(QStringLiteral("Allow window activation from JavaScript"));
+        QCheckBox *showScrollBars = new QCheckBox(QStringLiteral("Show scroll bars"));
+        QCheckBox *playbackRequiresUserGesture = new QCheckBox(QStringLiteral("Playback requires user gestures"));
+        QCheckBox *javaScriptCanPaste = new QCheckBox(QStringLiteral("JavaScript can paste"));
+        QCheckBox *webRTCPublicInterfacesOnly = new QCheckBox(QStringLiteral("Limit WebRTC to public IP addresses only"));
+        QCheckBox *dnsPrefetchEnabled = new QCheckBox(QStringLiteral("DNS prefetch enabled"));
+
+        vboxLayout->addWidget(autoLoadImages);
+        vboxLayout->addWidget(javaScriptEnabled);
+        vboxLayout->addWidget(javaScriptCanOpenWindows);
+        vboxLayout->addWidget(javaScriptCanAccessClipboard);
+        vboxLayout->addWidget(linksIncludedInFocusChain);
+        vboxLayout->addWidget(localStorageEnabled);
+        vboxLayout->addWidget(localContentCanAccessRemoteUrls);
+        vboxLayout->addWidget(xssAuditingEnabled);
+        vboxLayout->addWidget(spatialNavigationEnabled);
+        vboxLayout->addWidget(localContentCanAccessFileUrls);
+        vboxLayout->addWidget(hyperLinkAuditingEnabled);
+        vboxLayout->addWidget(scrollAnimatorEnabled);
+        vboxLayout->addWidget(errorPageEnabled);
+        vboxLayout->addWidget(pluginsEnabled);
+        vboxLayout->addWidget(fullScreenSupportEnabled);
+        vboxLayout->addWidget(screenCaptureEnabled);
+        vboxLayout->addWidget(webglEnabled);
+        vboxLayout->addWidget(accelerated2DCanvasEnabled);
+        vboxLayout->addWidget(autoLoadImages);
+        vboxLayout->addWidget(autoLoadIconsForPage);
+        vboxLayout->addWidget(touchIconsEnabled);
+        vboxLayout->addWidget(focusOnNavigationEnabled);
+        vboxLayout->addWidget(printElementBackgrounds);
+        vboxLayout->addWidget(allowRunningInsecureContent);
+        vboxLayout->addWidget(allowGeolocationOnInsecureOrigin);
+        vboxLayout->addWidget(allowWindowActivationFromJavaScript);
+        vboxLayout->addWidget(showScrollBars);
+        vboxLayout->addWidget(playbackRequiresUserGesture);
+        vboxLayout->addWidget(javaScriptCanPaste);
+        vboxLayout->addWidget(webRTCPublicInterfacesOnly);
+        vboxLayout->addWidget(dnsPrefetchEnabled);
+    }
+
+    vboxLayout->addWidget(area, 1);
+    QHBoxLayout *hboxLayout = new QHBoxLayout;
+    QPushButton *restoreButton = new QPushButton(QStringLiteral("Restore to defaults"));
+    QPushButton *saveButton = new QPushButton(QStringLiteral("Save"));
+    hboxLayout->addWidget(new QWidget, 1);
+    hboxLayout->addWidget(restoreButton, 0);
+    hboxLayout->addWidget(saveButton, 0);
+    vboxLayout->addLayout(hboxLayout);
+
+    //connect(saveButton, &QPushButton::clicked, this, [this] { emit webengineSaveRequested(); });
+    //connect(restoreButton, &QPushButton::clicked, this, [this] { emit webengineRestoreRequested(); });
+
     return widget;
 }
 
